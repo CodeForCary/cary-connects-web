053f301f8710c91d6a16701c5abb7ce57ed50750
Carter Robinson <cartertrobinson@gmail.com>
2018-10-10 11:57:20 -0500

    Refactored filterLocation to handle asynchronous data

--- src/components/NavigationMenu/filterLocation.js
+++ src/components/NavigationMenu/filterLocation.js
@@ -1,17 +1,20 @@
 import Data from "./Data.js";
+import axios from 'axios';
 
 export default function filterLocation(searchText, maxResults) {
-
-  return Data
-    .features.filter(location => {
-      if ((location.properties.name.toLowerCase().includes(searchText.toLowerCase()) || location.properties["marker-symbol"].toLowerCase().includes(searchText.toLowerCase()))) {
-        return true;
-      }
-      if (location.properties.name.includes(searchText)) {
-        return true;
-      }
-      return false;
-    })
-    .slice(0, maxResults);
-
+  return new Promise((resolve, reject) => {
+    axios.get(`http://codeforcary.org/parking/business.geojson`)
+      .then(res => JSON.parse(res.data))
+      .then(locations => locations.filter(location => {
+        if ((location.properties.name.toLowerCase().includes(searchText.toLowerCase()) || location.properties["marker-symbol"].toLowerCase().includes(searchText.toLowerCase()))) {
+          return true;
+        }
+        if (location.properties.name.includes(searchText)) {
+          return true;
+        }
+        return false;
+      }))
+      .then(results => resolve(results.slice(0, maxResults)))
+      .catch(err => reject(err));
+  });
 }

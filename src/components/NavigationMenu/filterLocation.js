021b6fe4083607c58764a98e6efc6bdcab647eec
McCauley Peeler <mccauleypeeler@gmail.com>
2018-10-10 14:56:49 -0400

    fixed filter function

--- src/components/NavigationMenu/filterLocation.js
+++ src/components/NavigationMenu/filterLocation.js
@@ -1,17 +1,19 @@
 import geoJSON from "./geoJSON.json";
+import axios from 'axios';
 
 export default function filterLocation(searchText, maxResults) {
-
-  return geoJSON
-    .filter(location => {
-      if ((location.name.toLowerCase().includes(searchText.toLowerCase()) || location.amenity.toLowerCase().includes(searchText.toLowerCase()))) {
-        return true;
-      }
-      if (location.name.includes(searchText)) {
-        return true;
-      }
-      return false;
-    })
-    .slice(0, maxResults);
-
+  return new Promise((resolve, reject) => {
+    axios.get(`https://cors-anywhere.herokuapp.com/http://codeforcary.org/parking/business.geojson`)
+      .then(locations => locations.data.features.filter(location => {
+        if ((location.properties.name.toLowerCase().includes(searchText.toLowerCase()) || location.properties["marker-symbol"].toLowerCase().includes(searchText.toLowerCase()))) {
+          return true;
+        }
+        if (location.properties.name.includes(searchText)) {
+          return true;
+        }
+        return false;
+      }))
+      .then(results => resolve(results.slice(0, maxResults)))
+      .catch(err => reject(err));
+  });
 }

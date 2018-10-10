cf417f7693dccd4b9cc401669dd0d61811adb006
McCauley Peeler <mccauleypeeler@gmail.com>
2018-10-10 15:09:22 -0400

    create heroku build

--- src/components/NavigationMenu/filterLocation.js
+++ src/components/NavigationMenu/filterLocation.js
@@ -17,3 +17,16 @@ export default function filterLocation(searchText, maxResults) {
       .catch(err => reject(err));
   });
 }
+
+{/*return Data
+  .features.filter(location => {
+    if ((location.properties.name.toLowerCase().includes(searchText.toLowerCase()) || location.properties["marker-symbol"].toLowerCase().includes(searchText.toLowerCase()))) {
+      return true;
+    }
+    if (location.properties.name.includes(searchText)) {
+      return true;
+    }
+    return false;
+  })
+  .slice(0, maxResults);
+*/}

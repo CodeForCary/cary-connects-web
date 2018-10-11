ee86d9dfa10bd07c1bd9516fa50b72dc6d426fc2
Claire Stolp <clairemstolp@gmail.com>
2018-10-11 08:38:54 -0400

    Created basic card compnoent

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+
+});
+
+const Contact = props => {
+
+  return(
+    <Grid>
+      
+    </Grid>
+  );
+}
+
+export default withStyles(styles)(Contact);

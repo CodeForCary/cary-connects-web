6c9e4c2005337e3c32c98cd9910894ddfe87dd87
McCauley Peeler <mccauleypeeler@gmail.com>
2018-10-10 20:07:20 -0400

    removed heroku

--- src/components/NavigationMenu/filterLocation.js
+++ src/components/NavigationMenu/filterLocation.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default function filterLocation(searchText, maxResults) {
   return new Promise((resolve, reject) => {
-    axios.get(`https://cors-anywhere.herokuapp.com/http://codeforcary.org/parking/business.geojson`)
+    axios.get(`http://codeforcary.org/parking/business.geojson`)
       .then(locations => locations.data.features.filter(location => {
         if ((location.properties.name.toLowerCase().includes(searchText.toLowerCase()) || location.properties["marker-symbol"].toLowerCase().includes(searchText.toLowerCase()))) {
           return true;

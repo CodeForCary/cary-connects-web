b63b776f95358c029157c6a2dc8147b1589459b5
McCauley Peeler <mccauleypeeler@gmail.com>
2018-10-16 08:55:07 -0400

    jd

--- src/templates/DefaultTemplate/index.js
+++ src/templates/DefaultTemplate/index.js
@@ -3,6 +3,14 @@ import NavigationMenu from 'src/components/NavigationMenu'
 import Map from 'src/components/Map'
 
 export default class DefaultTemplate extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state: {
+      data: null
+    }
+  }
+
   render () {
     return (
       <div id='default-template'>

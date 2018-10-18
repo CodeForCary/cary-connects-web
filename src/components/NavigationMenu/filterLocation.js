ba9be0fcd45f56c920b3a61ce98862f82a0ee325
McCauley Peeler <mccauleypeeler@gmail.com>
2018-10-18 19:03:11 -0400

    removed garbage

--- src/components/NavigationMenu/filterLocation.js
+++ src/components/NavigationMenu/filterLocation.js
@@ -15,16 +15,3 @@ export default function filterLocation(searchText, maxResults, data) {
     .slice(0, maxResults);
   }
 }
-
-{/*return Data
-  .features.filter(location => {
-    if ((location.properties.name.toLowerCase().includes(searchText.toLowerCase()) || location.properties["marker-symbol"].toLowerCase().includes(searchText.toLowerCase()))) {
-      return true;
-    }
-    if (location.properties.name.includes(searchText)) {
-      return true;
-    }
-    return false;
-  })
-  .slice(0, maxResults);
-*/}

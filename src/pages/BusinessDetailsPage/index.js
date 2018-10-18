cf893bf8d4a2ec02a2efb367accc328a2312d744
Chris Ellis <ellis.cs@gmail.com>
2018-10-18 19:30:24 -0400

    generateCards iterates businesses

--- src/pages/BusinessDetailsPage/index.js
+++ src/pages/BusinessDetailsPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import DefaultTemplate from 'src/templates/DefaultTemplate'
 import Card from 'src/components/Card'
 import PageDirectory from 'src/pages/PageDirectory'
@@ -6,7 +6,7 @@ import Link from 'src/elements/Link'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 
-const business = [{
+const businesses = [{
   name: 'Enter Business Title Here',
   address: '123 Oakwood Dr',
   phone: '(555)555-5555',
@@ -17,23 +17,17 @@ const business = [{
 {
   name: 'Another Business',
   address: '456 blueLake St'
-}]
+}];
 
-const generateCards = (data) => {
-  let arr = [];
-  data.forEach((val, i) => {
-    const item = <Card type='business' data={val} id={i} />
-    arr.push(item)
-  })
-  return arr;
-}
+const generateCards = (businesses) => {
+  return businesses.map((business) => {
+    return <Card type='business' data={business} key={business.name} />
+  });
+};
 
-
-
-export default class BusinessDetailsPage extends React.Component {
-  
+export default class BusinessDetailsPage extends Component {
   render () {
-    const cards = generateCards(business);
+    const cards = generateCards(businesses);
     return (
       <DefaultTemplate>
         Business Details Page for ID: { this.props.match.params.id }

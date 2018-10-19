fdbe3cc589da22897fa534dbce755fd3428205c5
Chris Ellis <ellis.cs@gmail.com>
2018-10-19 06:22:23 -0400

    Refactored actions to Provider/Consumer. State is in Provider.js

--- src/components/NavigationMenu/LocationResultRow.js
+++ src/components/NavigationMenu/LocationResultRow.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Context } from "../Provider";
 import "./LocationResultRow.css";
 
 class LocationResultsRow extends Component {
@@ -9,13 +10,18 @@ class LocationResultsRow extends Component {
 
   render() {
     return (
-      <div
-        className="component-location-result-row"
-        onClick={this.takeName.bind(this, this.props)}
-        >
-        <span className="name">{this.props.name}</span>
-        <span className="amenity"> <strong> {this.props["marker-symbol"]}</strong></span>
-      </div>
+      <Context.Consumer>
+        {(context) => (
+          <div
+            className="component-location-result-row"
+            // onClick={this.takeName.bind(this, this.props)}
+            onClick={context.relocater.bind(this, this.props)}
+          >
+            <span className="name">{this.props.name}</span>
+            <span className="amenity"> <strong> {this.props["marker-symbol"]}</strong></span>
+          </div>
+        )}
+      </Context.Consumer>
     );
   }
 }
--- src/templates/DefaultTemplate/index.js
+++ src/templates/DefaultTemplate/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import NavigationMenu from 'src/components/NavigationMenu'
-import Map from 'src/components/Map'
-import flip from 'geojson-flip';
+import React, { Component } from "react";
+import NavigationMenu from "src/components/NavigationMenu";
+import Map from "src/components/Map";
+import flip from "geojson-flip";
 
-import Provider, { Context } from '../../components/Provider';
+import Provider, { Context } from "../../components/Provider";
 
 export default class DefaultTemplate extends Component {
   constructor(props) {
@@ -14,60 +14,25 @@ export default class DefaultTemplate extends Component {
       parkingData: null,
       location: {
         lat: 35.787317,
-    		lng: -78.781226
+        lng: -78.781226
       },
-      zoom: 15,
+      zoom: 15
     };
   }
 
-  componentDidMount() {
-    return new Promise((resolve, reject) => {
-      fetch('https://raw.githubusercontent.com/CodeForCary/cary-connects-data/master/business.geojson')
-        .then(response => response.json())
-        .then(rawJSON => rawJSON.features)
-        .then(data => resolve(this.setState({ businessData: data })))
-        .catch(err => reject(err));
-      fetch('https://raw.githubusercontent.com/CodeForCary/cary-connects-data/master/parking.geojson')
-        .then(response => response.json())
-        .then(rawJSON => flip.flip(rawJSON.features))
-        .then(data => resolve(this.setState({ parkingData: data })))
-        .catch(err => reject(err));
-    });
-  }
-
-  relocater = (property) => {
-    let feature = this.state.businessData
-      .find(feature => feature.properties.name === property.name);
-    this.setState({
-      location: {
-        lat: feature.geometry.coordinates[1],
-        lng: feature.geometry.coordinates[0]
-      },
-      zoom: 18
-    });
-  };
-
-  moveMap(location, zoom) {
-    this.setState({
-      location: location,
-      zoom: zoom
-    });
-  }
-
-
-  render () {
-    const { businessData } = this.state;
-    const { parkingData } = this.state;
-    console.log(`from default: ${this.state}`)
+  render() {
     return (
       <Provider>
-        <div id='default-template'>
-          <NavigationMenu data={businessData} thirdNamePass={this.relocater} />
-          <Map relocateMap={this.state.location} zoomMap={this.state.zoom} modifyLocation={this.moveMap.bind(this)} polygonData={parkingData} />
-          {this.props.children}
-        </div>
+        <Context.Consumer>
+          {context => (
+            <div id="default-template">
+              <NavigationMenu data={context.state.businessData} />
+              <Map polygonData={context.state.parkingData} />
+              {this.props.children}
+            </div>
+          )}
+        </Context.Consumer>
       </Provider>
-    )
+    );
   }
-
 }

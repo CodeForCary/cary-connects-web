d00c61b78e63f576b2b993e0e93832f76563f229
McCauley Peeler <mccauleypeeler@gmail.com>
2018-10-23 14:32:51 -0400

    search resize

--- src/components/NavigationMenu/LocationResults.js
+++ src/components/NavigationMenu/LocationResults.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
 import './LocationResults.css';
 import PropTypes from "prop-types";
-
 import LocationResultRow from "./LocationResultRow";
 
 class LocationResults extends Component {
-
   render() {
     return (
+      <div className="component-location">
       <div className="component-location-results">
         {this.props.locationData.map(locationData => (
+          <div className="component-location-results-result">
           <LocationResultRow
             name={locationData.properties.name}
             amenity={locationData.properties["marker-symbol"]}
             key={locationData.properties.name}
           />
+          </div>
         ))}
+        </div>
       </div>
     );
   }

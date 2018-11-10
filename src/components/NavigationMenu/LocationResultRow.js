7a677aaaae8031c0f1e372dc8ee8fae393aa2c65
0jordanmiller@gmail.com <0jordanmiller@gmail.com>
2018-11-10 15:42:32 -0500

    markers on search click added

--- src/components/NavigationMenu/LocationResultRow.js
+++ src/components/NavigationMenu/LocationResultRow.js
@@ -4,17 +4,19 @@ import { Context } from "../Provider";
 import "./LocationResultRow.css";
 
 class LocationResultsRow extends Component {
-
   render() {
     return (
       <Context.Consumer>
-        {(context) => (
+        {context => (
           <div
             className="component-location-result-row"
             onClick={context.chooseBusiness.bind(this, this.props)}
           >
             <span className="name">{this.props.name}</span>
-            <span className="amenity"> <strong> {this.props["marker-symbol"]}</strong></span>
+            <span className="amenity">
+              {" "}
+              <strong> {this.props["marker-symbol"]}</strong>
+            </span>
           </div>
         )}
       </Context.Consumer>
@@ -22,6 +24,6 @@ class LocationResultsRow extends Component {
   }
 }
 LocationResultsRow.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string
 };
 export default LocationResultsRow;
--- src/components/NavigationMenu/results/ResultsList.js
+++ src/components/NavigationMenu/results/ResultsList.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import { Context } from '../../Provider'
-import capitalize from 'capitalize'
+import React, { Component } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { Context } from "../../Provider";
+import capitalize from "capitalize";
 
 export default class ResultsList extends Component {
-
   render() {
     return (
       <Context.Consumer>
         {context => (
           <List>
-            {this.props.data.map((location) => (
-              <ListItem button key={location.properties.name} name={location.properties.name} onClick={context.chooseBusiness.bind(this, location)}>
-                <ListItemText primary={location.properties.name} secondary={capitalize(location.properties["marker-symbol"])} />
+            {this.props.data.map(location => (
+              <ListItem
+                button
+                key={location.properties.name}
+                name={location.properties.name}
+                onClick={event => {
+                  context.chooseBusiness(location);
+                }}
+              >
+                <ListItemText
+                  primary={location.properties.name}
+                  secondary={capitalize(location.properties["marker-symbol"])}
+                />
               </ListItem>
             ))}
           </List>
-          )
-        }
+        )}
       </Context.Consumer>
-    )
+    );
   }
 }

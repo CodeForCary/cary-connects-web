175ac7578cef475d011e4a85fad593f2077bbf4d
Chris Ellis <ellis.cs@gmail.com>
2019-01-13 14:23:05 -0500

    Remove missing anchorEl warning after search result chosen.

--- client/src/components/Home/NavigationMenu/results/ResultsList.js
+++ client/src/components/Home/NavigationMenu/results/ResultsList.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import axios from 'axios';
+import axios from "axios";
 import ListItemText from "@material-ui/core/ListItemText";
 import { Context } from "src/components/Provider";
 import capitalize from "capitalize";
 
 export default class ResultsList extends Component {
-
   handleSubmit(name) {
-    axios.post("/api/updateData", {name: name})
-  };
+    axios.post("/api/updateData", { name: name });
+  }
 
   render() {
     return (
@@ -23,7 +22,8 @@ export default class ResultsList extends Component {
                 key={location.properties.name}
                 name={location.properties.name}
                 onClick={event => {
-                  context.chooseBusiness(location); this.handleSubmit(location.properties.name)
+                  context.chooseBusiness(location);
+                  this.handleSubmit(location.properties.name);
                 }}
               >
                 <ListItemText

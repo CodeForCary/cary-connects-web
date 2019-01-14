45c12030d925b0f4ca8bf2be3c84a97d5f6e7444
Chris Ellis <ellis.cs@gmail.com>
2019-01-14 06:39:17 -0500

    Upgrade Material UI, and migrate typography variants.

--- client/src/components/Home/Drawer/index.js
+++ client/src/components/Home/Drawer/index.js
@@ -1,48 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Context } from "src/components/Provider";
 
+import { withStyles } from "@material-ui/core/styles";
 
-import { withStyles } from '@material-ui/core/styles'
+import Drawer from "@material-ui/core/Drawer";
 
-import Drawer from '@material-ui/core/Drawer'
-
-import { Business, Parking } from './Content';
-
+import { Business, Parking } from "./Content";
 
 const drawerWidth = 340;
 
 const styles = theme => ({
   root: {
-    height: '100%',
+    height: "100%"
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    height: '100%',
+    height: "100%"
   },
   drawerPaper: {
-    position: 'relative',
-    paddingLeft: theme.spacing.unit * 2, 
+    position: "relative",
+    paddingLeft: theme.spacing.unit * 2,
     width: drawerWidth,
     backgroundColor: theme.palette.background.paper,
-    height: '100%',
-    padding: '24px 0'
+    height: "100%",
+    padding: "24px 0"
   },
   header: {
-    margin: '0 10px'
+    margin: "0 10px"
   }
 });
 
 const getContent = (type, data) => {
-  if(type === 'parking') {
-    return <Parking data={data[0].properties} coords={data[0].properties.entrance1.split(" ")} />
-  }else if(type === 'business') {
-    return <Business data={data.properties} coords={data.geometry.coordinates} />
+  if (type === "parking") {
+    return <Parking data={data[0].properties} coords={data[0].properties.entrance1.split(" ")} />;
+  } else if (type === "business") {
+    return <Business data={data.properties} coords={data.geometry.coordinates} />;
   }
-}
+};
 
 class index extends Component {
-
   render() {
     const { classes, data, type } = this.props;
 
@@ -62,10 +59,9 @@ class index extends Component {
               {context.state.drawerOpen ? getContent(type, data) : null}
             </Drawer>
           )}
-
         </Context.Consumer>
       </div>
-    )
+    );
   }
 }
 
--- client/src/components/Home/NavigationMenu/index.js
+++ client/src/components/Home/NavigationMenu/index.js
@@ -1,57 +1,55 @@
-import React, { Component } from 'react'
-import SearchInput from './SearchInput'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import CaryConnectsIcon from 'src/assets/CaryConnectIcon.png'
+import React, { Component } from "react";
+import SearchInput from "./SearchInput";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CaryConnectsIcon from "src/assets/CaryConnectIcon.png";
 import { Context } from "src/components/Provider";
-import Results from './results'
+import Results from "./results";
 
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   root: {
-    width: '100%',
+    width: "100%",
     marginBottom: theme.spacing.unit * 3
   },
   grow: {
-    flexGrow: '1'
+    flexGrow: "1"
   },
   icon: {
-    height: '50px',
-    marginRight: '5px'
+    height: "50px",
+    marginRight: "5px"
   },
   brandText: {
-    color: '#fff'
+    color: "#fff"
   },
   toolbar: theme.mixins.toolbar
-})
-
+});
 
 class NavigationMenu extends Component {
-
-  render () {
+  render() {
     const { classes } = this.props;
 
     return (
       <Context.Consumer>
-      {context => (
-      <div className={classes.root} >
-      <AppBar position="static" onClick={context.handleDrawerClose}>
-        <Toolbar>
-            <img className={classes.icon} src={CaryConnectsIcon} alt='cary connects icon' />
-            <Typography variant="title" className={classes.brandText}>
-              Cary Connects
-            </Typography>
-          <div className={classes.grow} />
-          <SearchInput textChange={context.handleSearchChange} />
-        </Toolbar>
-      </AppBar>
-      <Results locationData={context.state.filteredLocation} />
-      </div>
-      )}
+        {context => (
+          <div className={classes.root}>
+            <AppBar position='static' onClick={context.handleDrawerClose}>
+              <Toolbar>
+                <img className={classes.icon} src={CaryConnectsIcon} alt='cary connects icon' />
+                <Typography variant='h6' className={classes.brandText}>
+                  Cary Connects
+                </Typography>
+                <div className={classes.grow} />
+                <SearchInput textChange={context.handleSearchChange} />
+              </Toolbar>
+            </AppBar>
+            <Results locationData={context.state.filteredLocation} />
+          </div>
+        )}
       </Context.Consumer>
-    )
+    );
   }
 }
 

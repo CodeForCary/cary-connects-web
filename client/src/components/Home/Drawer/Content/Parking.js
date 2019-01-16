23ac9d6ab1345ef79c7b577e9ac4d87d865c207e
Chris Ellis <ellis.cs@gmail.com>
2019-01-15 20:15:17 -0500

    Title to h6 per MUI Typography

--- client/src/components/Home/Drawer/Content/Parking.js
+++ client/src/components/Home/Drawer/Content/Parking.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { dataList } from './dataList';
+import { dataList } from "./dataList";
 
 //Material-UI
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Button from '@material-ui/core/Button';
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Button from "@material-ui/core/Button";
 
-const styles = theme => ({
+const styles = theme => ({});
 
-});
-
-const openGoogleMaps = (coords) => {
-  console.log(coords)
+const openGoogleMaps = coords => {
+  console.log(coords);
   window.open(
     "https://www.google.com/maps/dir/?api=1&destination=" +
       coords[1] +
       "," +
       coords[0].substring(0, coords[0].length - 1)
-  )
-}
-
+  );
+};
 
 class Parking extends Component {
-
   render() {
     const lot = this.props.data;
     const list = dataList(lot);
     return (
       <div>
-        <Typography variant="title">
-          Parking: <br />{lot.name}
+        <Typography variant='h6'>
+          Parking: <br />
+          {lot.name}
           <List>
             {list}
             <ListItem>
-              <Button
-                variant="outlined"
-                onClick={() => (openGoogleMaps(this.props.coords))}
-              >
+              <Button variant='outlined' onClick={() => openGoogleMaps(this.props.coords)}>
                 Directions
               </Button>
             </ListItem>
           </List>
         </Typography>
       </div>
-    )
+    );
   }
 }
 

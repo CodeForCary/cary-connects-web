4dfcab9c0435900a24b3bbc6f603b773a6cabeca
Sowmya <sowmya.aji@gmail.com>
2019-02-06 20:36:20 -0500

    Change background color to Welcome page, change font size and color and some formatting

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -6,16 +6,16 @@ import FadeIn from "react-fade-in";
 const styles = theme => ({
   root: {
     textAlign: "center",
-    backgroundColor: "#0190BE",
+    backgroundColor: "#d0e2aa",
     height: "100%",
     width: "100%",
     position: "absolute"
   },
   title: {
-    paddingTop: "23%",
+    paddingTop: "10%",
     fontFamily: "sans-serif",
     fontWeight: "100",
-    fontSize: "72px",
+    fontSize: "70px",
     color: "white"
   }
 });
@@ -51,12 +51,17 @@ class Welcome extends Component {
       <div className={classes.root} >
         <FadeIn transitionDuration={5000}>
           <h1 className={classes.title}>Welcome to our Cary Downtown Parking site!</h1>
-          <center><font face="sans-serif" size="4" color="white"></font><a href="http://codeforcary.org/parking"><font face="sans-serif" size="4" color="#D0D3D4">Click here for more info</font></a></center>
-          <center><font face="sans-serif" size="10" color="white">The first app for our Cary Connects platform</font></center>
-          <center><font face="sans-serif" size="4" color="white">Created by Volunteers</font></center>
-          <center><font face="sans-serif" size="4" color="white">with the </font><a href="https://www.heartofcary.org/caryapp.html"><font face="sans-serif" size="4" color="#D0D3D4">Heart of Cary</font></a></center>
-          <center><font face="sans-serif" size="4" color="white">and</font></center>
-          <center><font face="sans-serif" size="4" color="white"></font><a href="http://codeforcary.org/parking/"><font face="sans-serif" size="4" color="#D0D3D4">Code for Cary</font></a></center>
+          <center><font face="sans-serif" size="5" color="whitesmoke"></font><a href="http://codeforcary.org/parking"><font face="sans-serif" size="5" color="#1a1a1c"><i>Click here for more info</i></font></a></center>
+          <br></br>
+          <center><font face="sans-serif" size="8" color="white">The first app for our Cary Connects platform</font></center>
+          <br>
+          </br>
+          <br></br>
+          <center><font face="sans-serif" size="5" color="white">Created by Volunteers</font></center>
+          <br></br>
+          <center><font face="sans-serif" size="5" color="white">with the </font><a href="https://www.heartofcary.org/caryapp.html"><font face="sans-serif" size="5" color="#1a1a1c">Heart of Cary</font></a></center>
+          <center><font face="sans-serif" size="5" color="white">and</font></center>
+          <center><font face="sans-serif" size="5" color="white"></font><a href="http://codeforcary.org/parking/"><font face="sans-serif" size="5" color="#1a1a1c">Code for Cary</font></a></center>
         </FadeIn>
       </div>
     );

528184374afcd3af12d6eb292fb92704ac19c4d1
Jordan <0jordanmiller@gmail.com>
2019-02-06 19:37:01 -0500

    welcome page

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -12,11 +12,27 @@ const styles = theme => ({
     position: "absolute"
   },
   title: {
-    paddingTop: "23%",
+    paddingTop: "10%",
     fontFamily: "sans-serif",
     fontWeight: "100",
-    fontSize: "72px",
+    fontSize: "6vh",
     color: "white"
+  },
+  subtitle: {
+    fontSize: "4vh",
+    color: "white"
+  },
+  anchor: {
+    color: "white"
+  },
+  verticalCenter: {
+    transform: "translateY(-50%)"
+  },
+  wrapper: {
+    display: "flex",
+    "align-items": "center",
+    "justify-content": "center",
+    height: "100vh"
   }
 });
 
@@ -45,13 +61,55 @@ class Welcome extends Component {
     const { classes } = this.props;
 
     if (this.state.redirect) {
-      return <Redirect push to='/home' />;
+      return <Redirect push to="/home" />;
     }
+
     return (
       <div className={classes.root}>
-        <FadeIn transitionDuration={400}>
-          <h1 className={classes.title}>Welcome to Cary Connects!</h1>
-        </FadeIn>
+        <div className={classes.wrapper}>
+          <FadeIn transitionDuration={5000} className={classes.verticalCenter}>
+            <h1 className={classes.title}>
+              Welcome to our Cary Downtown Parking site!
+            </h1>
+            <div className={classes.subtitle}>
+              <center>
+                <a
+                  className={classes.anchor}
+                  href="http://codeforcary.org/parking"
+                >
+                  <font>About</font>
+                </a>
+              </center>
+              <center>
+                <font>The first app for our Cary Connects platform</font>
+              </center>
+              <center>
+                <font>Created by Volunteers</font>
+              </center>
+              <center>
+                <font>with the </font>
+                <a
+                  className={classes.anchor}
+                  href="https://www.heartofcary.org/caryapp.html"
+                >
+                  Heart of Cary
+                </a>
+              </center>
+              <center>
+                <font>and</font>
+              </center>
+              <center>
+                <font />
+                <a
+                  className={classes.anchor}
+                  href="http://codeforcary.org/parking/"
+                >
+                  <font>Code for Cary</font>
+                </a>
+              </center>
+            </div>
+          </FadeIn>
+        </div>
       </div>
     );
   }

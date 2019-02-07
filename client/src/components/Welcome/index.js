ac2f7a16fd4f3b65cf01801eec727e61313ae0d7
McCauley Peeler <mccauleypeeler@gmail.com>
2019-02-06 19:13:23 -0500

    welcome screen

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -48,9 +48,15 @@ class Welcome extends Component {
       return <Redirect push to='/home' />;
     }
     return (
-      <div className={classes.root}>
-        <FadeIn transitionDuration={400}>
-          <h1 className={classes.title}>Welcome to Cary Connects!</h1>
+      <div className={classes.root} >
+        <FadeIn transitionDuration={5000}>
+          <h1 className={classes.title}>Welcome to our Cary Downtown Parking site!</h1>
+          <center><font face="sans-serif" size="4" color="white"></font><a href="http://codeforcary.org/parking"><font face="sans-serif" size="4" color="#D0D3D4">Click here for more info</font></a></center>
+          <center><font face="sans-serif" size="10" color="white">The first app for our Cary Connects platform</font></center>
+          <center><font face="sans-serif" size="4" color="white">Created by Volunteers</font></center>
+          <center><font face="sans-serif" size="4" color="white">with the </font><a href="https://www.heartofcary.org/caryapp.html"><font face="sans-serif" size="4" color="#D0D3D4">Heart of Cary</font></a></center>
+          <center><font face="sans-serif" size="4" color="white">and</font></center>
+          <center><font face="sans-serif" size="4" color="white"></font><a href="http://codeforcary.org/parking/"><font face="sans-serif" size="4" color="#D0D3D4">Code for Cary</font></a></center>
         </FadeIn>
       </div>
     );
